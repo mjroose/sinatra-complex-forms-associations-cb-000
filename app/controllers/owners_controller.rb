850d917fac0b0eef4cfacb5c94868b52debc5060
Michael Roose <mjroose@gmail.com>
2018-11-01 18:16:24 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -20,7 +20,7 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    
+
     erb :'/owners/edit'
   end
 
