6d6237089515c93061d487f2a2cd8bb87845f95b
Michael Roose <mjroose@gmail.com>
2018-11-01 18:18:14 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,15 +12,15 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    
     redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
-    binding.pry
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
 
     erb :'/owners/edit'
   end
